1ebe46b2d31bc5205a91d72df059d152dad0ad78
iliasabai09 <iliasabaimain@gmail.com>
2025-09-21 21:42:00 +0500

    feat/remove unused components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {createApp} from 'vue'
-import './style.css'
+import './assets/styles/global.scss'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
